78ca124571dee07e6a59111a032ea3854dac22ce
jingyi <niu.jy1203@gmail.com>
2023-08-28 14:35:09 +1200

    create project with vite and populate data

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import App from "./app/layout/App.tsx";
+import "semantic-ui-css/semantic.min.css";
+import "./app/layout/styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
